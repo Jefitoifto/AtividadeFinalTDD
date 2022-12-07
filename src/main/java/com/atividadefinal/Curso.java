30379735e1aed48d77e0b857b3bffada136b02f7
Jeferson Oliveira <81529357+Jefitoifto@users.noreply.github.com>
2022-12-07 11:30:40 -0300

    Update Curso.java

--- src/main/java/com/atividadefinal/Curso.java
+++ src/main/java/com/atividadefinal/Curso.java
@@ -5,6 +5,27 @@ public class Curso {
     String cargaHoraria;
     String descricao;
     void Curso(String nome, String cargaHoraria, String descricao){
-
+        this.nome = nome;
+        this.cargaHoraria = cargaHoraria;
+        this.descricao = descricao;
     }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCargaHoraria() {
+        return cargaHoraria;
+    }
+    public void setCargaHoraria(String cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
 }
