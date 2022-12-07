58f1f835b4768bca15c2f1f12adc7f9450dc50cc
Jeferson Oliveira <81529357+Jefitoifto@users.noreply.github.com>
2022-12-07 11:31:21 -0300

    Update turmaCurso.java

--- src/main/java/com/atividadefinal/turmaCurso.java
+++ src/main/java/com/atividadefinal/turmaCurso.java
@@ -11,17 +11,81 @@ public class turmaCurso {
    Date inicioMatriculas;
    Date fimMatriculas;
 
-   void turmaCurso(String local, int vagas, Date inicioAulas, Date fimAulas, Date inicioMatricula, Date fimMatricula){
+   void turmaCurso(String local, int vagas, Date inicioAulas, 
+         Date fimAulas, Date inicioMatricula, Date fimMatricula, int vagasDisponiveis,
+         Date inicioMatriculas, Date fimMatriculas){
+            
+      this.local = local;
+      this.vagas = vagas;
+      this.vagasDisponiveis = vagasDisponiveis;
+      this.inicioAulas = inicioAulas;
+      this.fimAulas = fimAulas;
+      this.inicioMatriculas = inicioMatriculas;
+      this.fimMatriculas = fimMatriculas;
+   }
 
+   public turmaCurso(String local, int vagas, int vagasDisponiveis, Date inicioAulas, Date fimAulas,
+         Date inicioMatriculas, Date fimMatriculas) {
+      
+   }
+
+   public String getLocal() {
+      return local;
+   }
+
+   public void setLocal(String local) {
+      this.local = local;
+   }
+
+   public int getVagas() {
+      return vagas;
+   }
+
+   public void setVagas(int vagas) {
+      this.vagas = vagas;
+   }
+
+   public void setVagasDisponiveis(int vagasDisponiveis) {
+      this.vagasDisponiveis = vagasDisponiveis;
+   }
+
+   public Date getInicioAulas() {
+      return inicioAulas;
+   }
+
+   public void setInicioAulas(Date inicioAulas) {
+      this.inicioAulas = inicioAulas;
+   }
+
+   public Date getFimAulas() {
+      return fimAulas;
+   }
+
+   public void setFimAulas(Date fimAulas) {
+      this.fimAulas = fimAulas;
+   }
+
+   public Date getInicioMatriculas() {
+      return inicioMatriculas;
+   }
+
+   public void setInicioMatriculas(Date inicioMatriculas) {
+      this.inicioMatriculas = inicioMatriculas;
+   }
+
+   public Date getFimMatriculas() {
+      return fimMatriculas;
+   }
+
+   public void setFimMatriculas(Date fimMatriculas) {
+      this.fimMatriculas = fimMatriculas;
    }
 
    int getVagasDisponiveis(){
     return vagasDisponiveis;
    }
    void setVagasDisponiveis(){
-
    }
-
    Date getPeriodoMatriculas(){
     return inicioMatriculas;
    }
